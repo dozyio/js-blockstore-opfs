606f801720d6bd210e4c9bbd6f172bf96b614fa8
dozyio <github@dozy.io>
2024-10-29 02:44:49 +0000

    chore: remove exports to implementations, update main

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 // src/index.ts
-
-import { OPFSMainThreadFS } from './main-thread-fs.js'
 import { type OPFSBlockstoreInit, OPFSBlockstore } from './opfs-blockstore.js'
-import { OPFSWebWorkerFS } from './web-worker-fs.js'
 
-export { type OPFSBlockstoreInit, OPFSBlockstore, OPFSMainThreadFS, OPFSWebWorkerFS }
+export { type OPFSBlockstoreInit, OPFSBlockstore }
